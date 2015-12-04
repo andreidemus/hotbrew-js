64ad0954be92b2de1ef29f840043fcc48e31f726
Andrei Demus <andreidemus@cloud.upwork.com>
2015-12-04 22:53:03 +0200

    Resolving dependencies

--- Test.java
+++ Test.java
@@ -3,10 +3,15 @@
 */
 
 import java.util.stream.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 
 class Test {
     public static void main(String[] args) {
-        System.out.println("java test");
         Stream.of(args).forEach(System.out::println);
+
+        Client client = ClientBuilder.newClient();
+        String response = client.target("https://api.github.com/zen").request().get(String.class);
+        System.out.println(response);
     }
 }
--- j.js
+++ j.js
@@ -14,42 +14,82 @@ const filename = process.argv[2];
 const exec = require('child_process').execSync;
 const fs = require('fs');
 
-function compile(filename) {
-	exec('javac ' + filename);
+function compile(filename, cp) {
+	let cmd = 'javac ' + filename;
+	if (cp) {
+		cmd += ' -cp ' + cp;
+	}
+	exec(cmd);
 }
 
-function run(filename, args) {
+function run(filename, args, cp) {
 	const shortname = filename.split('.')[0];
-	const cmd = 'java ' + shortname + ' ' + args.join(' ');
+	let cmd = 'java ';
+	cmd += ' -cp "./:' + cp + '" ';
+	cmd += shortname + ' ' + args.join(' ');
 	return exec(cmd);
 }
 
-function resolveDeps(filename) {
+function createPom(deps) {
+	let dependencies = deps.map(i => 
+	`
+    <dependency>
+        <groupId>${i['groupId']}</groupId>
+        <artifactId>${i['artifactId']}</artifactId>
+        <version>${i['version']}</version>
+    </dependency>`)
+	.join('');
+
+	return `
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/POM/4.0.0
+                      http://maven.apache.org/xsd/maven-4.0.0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+   <groupId>hotbrew</groupId>
+  <artifactId>a-script</artifactId>
+  <version>1.0.0</version>
+  <packaging>pom</packaging>
+  <dependencies>
+    ${dependencies}
+  </dependencies>
+</project>
+    `;
+}
+
+function parseDeps(filename) {
+	console.log('** Searching for dependencies...');
 	const re = /(?:\/\*\*\*)([\s\S]*)(?:\*\/)/m;
 	const file = fs.readFileSync(filename, "utf8");
 	const m = file.match(re);
 	if (m) {
-		const deps = eval(m[1]);
-		deps.forEach(d => {
-			let path = repo + pathFromDep(d);
-			console.log(path);
-			//todo
-		});		
+		const deps = eval(m[1]).map(i => {
+			const parts = i.split(':');
+			return {
+				'groupId': parts[0],
+				'artifactId': parts[1],
+				'version': parts[2]
+			}
+		});
+		console.log('** ' + deps.length + ' dependencies found');
+		return deps;
 	}
+	console.log('** No dependencies found.');
 }
 
-function pathFromDep(str) {
-	const parts = str.split(':');
-	const group = parts[0];
-	const name = parts[1];
-	const ver = parts[2];
+function resolveDeps(filename) {
+	const deps = parseDeps(filename);
+	if (!deps) return;
 
-	const path = group.split('.').join('/');
-	return path + '/' + name + '/' + ver + '/' + name + '-' + ver + '.jar';
+	const pom = createPom(deps);
+	fs.writeFileSync('pom.xml', pom);
+	console.log('** Resolving dependencies...');
+	exec('mvn dependency:build-classpath -Dmdep.outputFile="cp.txt"');
+	console.log('** Done.');
 }
-
 
 resolveDeps(filename);
-compile(filename);
-const out = run(filename, process.argv.slice(3));
+const cp = fs.readFileSync('cp.txt', 'utf8');
+compile(filename, cp);
+const out = run(filename, process.argv.slice(3), cp);
 console.log(out.toString('utf8'));
